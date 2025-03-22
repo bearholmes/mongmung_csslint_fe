74b75830c389776488d357d81ba38a2e75ef18a9
bearholmes <bearholmes@gmail.com>
2025-03-22 19:31:28 +0900

    add : dist

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    viteCompression({
-      algorithm: 'brotliCompress', // Brotli 압축
-      ext: '.br',
-    }),
-    viteCompression({
-      algorithm: 'gzip',
-      ext: '.gz'
-    })
   ],
   server: {
     port: 5001,
